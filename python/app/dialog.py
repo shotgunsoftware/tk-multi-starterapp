b629bffa22111e7a72b356e8b23b3f76b1e9058d
manne ohrstrom <manne@shotgunsoftware.com>
2013-12-12 16:03:15 +0000

    Initial Commit.

---
+++ python/app/dialog.py
@@ -0,0 +1,63 @@
+# Copyright (c) 2013 Shotgun Software Inc.
+# 
+# CONFIDENTIAL AND PROPRIETARY
+# 
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your 
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+import sgtk
+import os
+import sys
+import threading
+
+# by importing QT from sgtk rather than directly, we ensure that
+# the code will be compatible with both PySide and PyQt.
+from sgtk.platform.qt import QtCore, QtGui
+from .ui.dialog import Ui_Dialog
+
+def show_dialog(app_instance):
+    """
+    Shows the main dialog window.
+    """
+    # in order to handle UIs seamlessly, each toolkit engine has methods for launching
+    # different types of windows. By using these methods, your windows will be correctly
+    # decorated and handled in a consistent fashion by the system. 
+    
+    # we pass the dialog class to this method and leave the actual construction
+    # to be carried out by toolkit.
+    app_instance.engine.show_dialog("Starter Template App...", app_instance, AppDialog)
+    
+
+
+class AppDialog(QtGui.QWidget):
+    """
+    Main application dialog window
+    """
+    
+    def __init__(self):
+        """
+        Constructor
+        """
+        # first, call the base class and let it do its thing.
+        QtGui.QWidget.__init__(self)
+        
+        # now load in the UI that was created in the UI designer
+        self.ui = Ui_Dialog() 
+        self.ui.setupUi(self)
+        
+        # most of the useful accessors are available through the Application class instance
+        # it is often handy to keep a reference to this. You can get it via the following method:
+        self._app = sgtk.platform.current_bundle()
+        
+        # via the self._app handle we can for example access:
+        # - The engine, via self._app.engine
+        # - A Shotgun API instance, via self._app.shotgun
+        # - A tk API instance, via self._app.tk 
+        
+        # lastly, set up our very basic UI
+        self.ui.context.setText("Current Context: %s" % self._app.context)
+        
+        
---
+++ python/app/ui/dialog.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'dialog.ui'
+#
+#      by: pyside-uic 0.2.13 running on PySide 1.1.0
+#
+# WARNING! All changes made in this file will be lost!
+
+from tank.platform.qt import QtCore, QtGui
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(431, 123)
+        self.horizontalLayout = QtGui.QHBoxLayout(Dialog)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.logo_example = QtGui.QLabel(Dialog)
+        self.logo_example.setText("")
+        self.logo_example.setPixmap(QtGui.QPixmap(":/res/sg_logo.png"))
+        self.logo_example.setObjectName("logo_example")
+        self.horizontalLayout.addWidget(self.logo_example)
+        self.context = QtGui.QLabel(Dialog)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.context.sizePolicy().hasHeightForWidth())
+        self.context.setSizePolicy(sizePolicy)
+        self.context.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
+        self.context.setObjectName("context")
+        self.horizontalLayout.addWidget(self.context)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "The Current Sgtk Environment", None, QtGui.QApplication.UnicodeUTF8))
+        self.context.setText(QtGui.QApplication.translate("Dialog", "Your Current Context: ", None, QtGui.QApplication.UnicodeUTF8))
+
+from . import resources_rc
